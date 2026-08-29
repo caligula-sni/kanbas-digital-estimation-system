61a869868be6e0a35f69f4a89f9da0cfb45f2738
tim <Janpascual143@gmail.com>
2026-08-29 13:41:29 +0800

    Delete src directory

--- src/includes/nav.php
+++
@@ -1,8 +0,0 @@
-<nav class="sidebar">
-    <div class="sidebar-title">KDES</div>
-    <a href="home.php" class="<?= basename($_SERVER['PHP_SELF']) === 'home.php' ? 'active' : '' ?>">Home</a>
-    <a href="items.php" class="<?= basename($_SERVER['PHP_SELF']) === 'items.php' ? 'active' : '' ?>">Search Materials</a>
-    <a href="kanbas_new.php" class="<?= in_array(basename($_SERVER['PHP_SELF']), ['kanbas_new.php', 'kanbas_build.php']) ? 'active' : '' ?>">Create Kanbas</a>
-    <a href="saved_kanbas.php" class="<?= basename($_SERVER['PHP_SELF']) === 'saved_kanbas.php' ? 'active' : '' ?>">Saved Kanbas</a>
-    <a href="logout.php" class="logout-link">Log Out</a>
-</nav>
